804106b0ed947ac281b4b5010d53462dfbe46a5b
poziomka <poziomkaa@gmail.com>
2018-10-27 19:59:44 +0200

    Add concat and sourcemaps to babel

--- gulpTasks/babel.js
+++ gulpTasks/babel.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 
 module.exports = function () {
     gulp.task('babel', function() {
         return gulp.src('./src/js/*.js')
+            .pipe(sourcemaps.init())
             .pipe(babel({
                 presets: ['@babel/env']
             }))
+            .pipe(concat('app.js'))
+            .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest('./build/js'));
     });
 }
