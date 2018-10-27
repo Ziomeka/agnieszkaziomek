ea75bcfb942d4475d377d7004c48a51e5ca11d21
poziomka <poziomkaa@gmail.com>
2018-10-27 19:53:18 +0200

    Add sourcemaps to project

--- gulpTasks/styles.js
+++ gulpTasks/styles.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
 module.exports = function () {
     gulp.task('styles', function() {
         return gulp.src('./src/sass/main.scss')
+            .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
+            .pipe(sourcemaps.write())
             .pipe(gulp.dest('././build/css'));
     });
 }
