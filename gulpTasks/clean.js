cc5c699db459592df65da81f1e896de886e585b3
Agnieszka Ziomek <a.ziomek@nowaera.pl>
2020-01-23 13:03:25 +0100

    Remove old setup files

--- gulpTasks/clean.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const config = require('../config.json');
-const clean = require('gulp-clean');
-module.exports = function () {
-    gulp.task('clean', function () {
-        return gulp.src(config.dest.catalog, {read: false})
-        .pipe(clean());
-    });
-}
