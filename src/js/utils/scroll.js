711a87fc1405cb13d87768ceb401d6fb37035f89
Agnieszka Ziomek <a.ziomek@nowaera.pl>
2020-01-28 16:35:24 +0100

    Refactor sroll util to be more flexible

--- src/js/utils/scroll.js
+++ src/js/utils/scroll.js
@@ -1,26 +1,33 @@
-const scroll = (element, offset, duration) => {
-    const startPositionY = window.scrollY;
-    const destinationPositionY = element.getBoundingClientRect().top - offset + window.scrollY;
-    const distance = destinationPositionY - startPositionY;
-    let startTime = null;
-    const ease = (t, b, c, d) => c * t / d + b;
-    const animate = currentTime => {
-        if (startTime === null) {
-            startTime = currentTime;
+export default {
+    scrollToElement(element, offset, duration) {
+        const startPositionY = window.scrollY;
+        const destinationPositionY = element.getBoundingClientRect().top - offset + window.scrollY;
+        const distance = destinationPositionY - startPositionY;
+        let startTime = null;
+        const ease = (t, b, c, d) => c * t / d + b;
+        const animate = currentTime => {
+            if (startTime === null) {
+                startTime = currentTime;
+            }
+            const timeElapsed = currentTime - startTime;
+            let step = ease(timeElapsed, startPositionY, distance, duration);
+            if (destinationPositionY > startPositionY && step > destinationPositionY) {
+                step = destinationPositionY;
+            }
+            if (destinationPositionY < startPositionY && step < destinationPositionY) {
+                step = destinationPositionY;
+            }
+            window.scrollTo(0, step);
+            if (step !== destinationPositionY) {
+                requestAnimationFrame(animate);
+            }
+        };
+        requestAnimationFrame(animate);
+    },
+    scrollToElementById(id, offset, duration) {
+        const item = document.getElementById(id);
+        if (item) {
+            this.scrollToElement(item, offset, duration);
         }
-        const timeElapsed = currentTime - startTime;
-        let step = ease(timeElapsed, startPositionY, distance, duration);
-        if (destinationPositionY > startPositionY && step > destinationPositionY) {
-            step = destinationPositionY;
-        }
-        if (destinationPositionY < startPositionY && step < destinationPositionY) {
-            step = destinationPositionY;
-        }
-        window.scrollTo(0, step);
-        if (step !== destinationPositionY) {
-            requestAnimationFrame(animate);
-        }
-    };
-    requestAnimationFrame(animate);
+    },
 };
-export { scroll };
