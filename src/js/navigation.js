2650fb9c12cfde47091aa4e5c6f2f20f73841fe8
Agnieszka Ziomek <a.ziomek@nowaera.pl>
2020-01-28 16:41:43 +0100

    Simplify navigation function

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,4 +1,4 @@
-import { scroll } from './utils/scroll';
+import scroll from './utils/scroll';
 const navigation = function() {
     const menu = {
         height: 100,
@@ -6,28 +6,22 @@ const navigation = function() {
         expandedClass: 'navigation--expanded',
     }
 
-    const goTo = (link) => {
-        const target=document.querySelector(link);
-        scroll(target, menu.height, 800);
-    }
-
-    const navigate = () => {
-        const root = document.querySelector(menu.rootElement);
-        root.addEventListener('click', (event) => {
-            switch(event.target.dataset.action) {
-            case 'expand':
-                root.classList.add(menu.expandedClass);
-                break;
-            case 'fold':
-                root.classList.remove(menu.expandedClass);
-                break;
-            case 'go-to':
-                event.preventDefault;
-                root.classList.remove(menu.expandedClass);
-                goTo(event.target.dataset.target);
-            }
-        });
-    }
-    navigate();
+    const root = document.querySelector(menu.rootElement);
+    root.addEventListener('click', (event) => {
+        const action = event.target.dataset.action;
+        switch(action) {
+        case 'expand':
+            root.classList.add(menu.expandedClass);
+            break;
+        case 'fold':
+            root.classList.remove(menu.expandedClass);
+            break;
+        case 'go-to':
+            event.preventDefault;
+            root.classList.remove(menu.expandedClass);
+            const targetId = event.target.dataset.target.slice(1)
+            scroll.scrollToElementById(targetId, menu.height, 800);
+        }
+    });
 };
 export { navigation };
